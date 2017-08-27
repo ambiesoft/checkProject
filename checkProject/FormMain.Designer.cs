9be5dfcd0962c1f9009eb9cc724bb168027ff95a
trueff <ambiesoft.trueff@gmail.com>
2017-08-28 00:10:21 +0900

    start

---
+++ checkProject/FormMain.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace checkProject
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtFilter = new System.Windows.Forms.TextBox();
+            this.lblDirectory = new System.Windows.Forms.Label();
+            this.lblFilter = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtResult
+            // 
+            this.txtResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResult.Location = new System.Drawing.Point(12, 98);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResult.Size = new System.Drawing.Size(536, 172);
+            this.txtResult.TabIndex = 0;
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Location = new System.Drawing.Point(66, 3);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.Size = new System.Drawing.Size(443, 20);
+            this.txtFolder.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(515, 0);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(33, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "&...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 69);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "&Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(473, 69);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtFilter
+            // 
+            this.txtFilter.Location = new System.Drawing.Point(66, 29);
+            this.txtFilter.Name = "txtFilter";
+            this.txtFilter.Size = new System.Drawing.Size(443, 20);
+            this.txtFilter.TabIndex = 5;
+            // 
+            // lblDirectory
+            // 
+            this.lblDirectory.AutoSize = true;
+            this.lblDirectory.Location = new System.Drawing.Point(9, 6);
+            this.lblDirectory.Name = "lblDirectory";
+            this.lblDirectory.Size = new System.Drawing.Size(39, 13);
+            this.lblDirectory.TabIndex = 6;
+            this.lblDirectory.Text = "&Folder:";
+            // 
+            // lblFilter
+            // 
+            this.lblFilter.AutoSize = true;
+            this.lblFilter.Location = new System.Drawing.Point(9, 32);
+            this.lblFilter.Name = "lblFilter";
+            this.lblFilter.Size = new System.Drawing.Size(32, 13);
+            this.lblFilter.TabIndex = 6;
+            this.lblFilter.Text = "&Fi&lter:";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 282);
+            this.Controls.Add(this.lblFilter);
+            this.Controls.Add(this.lblDirectory);
+            this.Controls.Add(this.txtFilter);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtFolder);
+            this.Controls.Add(this.txtResult);
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtFilter;
+        private System.Windows.Forms.Label lblDirectory;
+        private System.Windows.Forms.Label lblFilter;
+    }
+}
+
---
+++ checkProject/FormMain.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace checkProject
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                checkProject(@"C:\T\Win32Project1\Win32Project1\Win32Project1.vcxproj");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        string getSafeAttribute(XmlNode node, string attr)
+        {
+            if (node.Attributes[attr] == null)
+                return string.Empty;
+
+            return node.Attributes[attr].Value;
+        }
+
+        void checkProject(string file)
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+
+            StringBuilder sb = new StringBuilder();
+
+            xmlDocument.Load(file);
+            var nsmgr = new XmlNamespaceManager(xmlDocument.NameTable);
+            nsmgr.AddNamespace("a", "http://schemas.microsoft.com/developer/msbuild/2003");
+
+            XmlNodeList nodeList = xmlDocument.SelectNodes("//a:PropertyGroup",nsmgr);
+
+            foreach(XmlNode node in nodeList)
+            {
+                if (getSafeAttribute(node,"Label") != "Configuration")
+                    continue;
+
+                string cond = node.Attributes["Condition"].Value;
+
+                                
+                if (cond.ToLower().IndexOf("debug") != -1)
+                {
+                    checkNode(node, sb, true);
+                }
+                else if (cond.ToLower().IndexOf("release") != -1)
+                {
+                    checkNode(node, sb, false);
+                }
+                else if (cond.ToLower().IndexOf("ship") != -1)
+                {
+                    checkNode(node, sb, false);
+                }
+                else
+                {
+                    sb.AppendLine("Unknown Node: " + cond);
+                }
+            }
+
+            appendResult(sb.ToString());
+        }
+
+        void appendResult(string s)
+        {
+            txtResult.Text += s;
+            txtResult.SelectionStart = txtResult.Text.Length; // add some logic if length is 0
+            txtResult.SelectionLength = 0;
+        }
+        void checkNode(XmlNode node, StringBuilder sb, bool isdebug)
+        {
+            XmlNode udl = null;
+            foreach (XmlNode cn in node.ChildNodes)
+            {
+                if (cn.Name == "UseDebugLibraries")
+                {
+                    udl = cn;
+                    break;
+                }
+            }
+
+            bool ok = false;
+            if (isdebug)
+            {
+                ok = udl != null && udl.InnerText == "true";
+            }
+            else
+            {
+                ok = udl != null && udl.InnerText == "false";
+            }
+
+            if (ok)
+            {
+                sb.Append("OK");
+            }
+            else
+            {
+                sb.Append("NG");
+            }
+            sb.Append(" : ");
+
+            string s = string.Format("{0}={1}", "Condition", node.Attributes["Condition"].Value);
+            s += " ";
+            s += string.Format("{0}={1}", "UseDebugLibraries", udl != null ? udl.InnerText : "NA");
+            sb.AppendLine(s);
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog dlg = new FolderBrowserDialog())
+            {
+                if (DialogResult.OK != dlg.ShowDialog())
+                    return;
+                txtFolder.Text = dlg.SelectedPath;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                doStart(new DirectoryInfo(txtFolder.Text));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        bool isFilterProject(string projname)
+        {
+            if (string.IsNullOrEmpty(txtFilter.Text))
+                return false;
+
+            if (-1 != projname.ToLower().IndexOf(txtFilter.Text.ToLower()))
+            {
+                // found text same as filter
+                return false;
+            }
+
+            return true;
+        }
+
+        void doStart(DirectoryInfo di)
+        {
+            foreach (FileInfo proj in di.GetFiles("*.vcxproj"))
+            {
+                if (isFilterProject(proj.Name))
+                    continue;
+                appendResult(string.Format("===== analyzing {0} =====\r\n", proj.FullName));
+                checkProject(proj.FullName);
+            }
+
+            foreach (DirectoryInfo childdi in di.GetDirectories())
+            {
+                doStart(childdi);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtResult.Text = string.Empty;
+        }
+    }
+}
